c299e5bd8d77f941ca9afb790338b2277c845498
theusccs111 <theusccs111@gmail.com>
2016-09-04 19:15:55 -0300

    04/09 projeto começando a integrar com os testes

--- APP_LifeTests/Models/usuarioTests.cs
+++ APP_LifeTests/Models/usuarioTests.cs
@@ -6,21 +6,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace APP_Life.Models.Tests
 {
     [TestClass()]
     public class usuarioTests
     {
+
+      
         [TestMethod()]
         public void CadastrarUsuarioTest()
         {
+            //ambiente   
             app_lifeContext contexto = new app_lifeContext();
             usuario teste = new usuario();
+            
             teste.email = "teste@teste.com";
             teste.senha = "12345";
+            //Execução
             teste.CadastrarUsuario(teste);
-            Assert.AreEqual(teste,);
+            var atual = (from u in contexto.usuarios select u).Take(1).OrderByDescending(x => x.usuarioID);
+            //teste
+            Assert.AreEqual(teste,atual);
 
 
 
