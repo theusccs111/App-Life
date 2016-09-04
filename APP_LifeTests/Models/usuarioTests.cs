e303cbb65706479f41e402436545a5f93302060c
Rafael <Rafael@RAFAEL>
2016-09-04 17:29:47 -0300

    Projeto de Testes criados

---
+++ APP_LifeTests/Models/usuarioTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APP_Life;
+using APP_Life.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_Life.Models.Tests
+{
+    [TestClass()]
+    public class usuarioTests
+    {
+        [TestMethod()]
+        public void CadastrarUsuarioTest()
+        {
+            app_lifeContext contexto = new app_lifeContext();
+            usuario teste = new usuario();
+            teste.email = "teste@teste.com";
+            teste.senha = "12345";
+            teste.CadastrarUsuario(teste);
+            Assert.AreEqual(teste,);
+
+
+
+        }
+    }
+}
--- APP_LifeTests/Properties/AssemblyInfo.cs
+++ APP_LifeTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("APP_Life")]
+[assembly: AssemblyTitle("APP_LifeTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("APP_Life")]
+[assembly: AssemblyProduct("APP_LifeTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9ca1fbec-06d0-491f-94b6-d7b596d474e7")]
+[assembly: Guid("e44bc566-de72-4c57-ac7b-d31221328bdc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("APP_Life.Tests")]
-[assembly: InternalsVisibleTo("APP_Life.Explorables")]
-
