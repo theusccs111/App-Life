15088d844b951ea05e0cb28787824d25d84f4bd3
theusccs111 <theusccs111@gmail.com>
2016-09-17 15:17:31 -0300

    Projeção

---
+++ app_life/Controllers/ProjetadoController.cs
@@ -0,0 +1,106 @@
+﻿using APP_Life.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APP_Life.Controllers
+{
+    public class ProjetadoController : Controller
+    {
+        app_lifeContext contexto = new app_lifeContext();
+        // GET: Projetado
+        public ActionResult Projetado()
+        {
+            if (Session["usuarioLogadoID"] != null)
+            {
+                ViewBag.listaProjetado = contexto.projetadoes.ToList();
+                ViewBag.CategoriaID = new SelectList
+                (
+                    contexto.categorias.ToList(),
+                    "CategoriaID",
+                    "nome"
+                );
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Inicio", "Login");
+            }
+
+        }
+
+
+        public ActionResult CadastrarProjetado()
+        {
+            ViewBag.CategoriaID = new SelectList
+                (
+                    contexto.categorias.ToList(),
+                    "CategoriaID",
+                    "nome"
+                );
+
+            return PartialView("_CadastrarProjetado");
+
+
+        }
+
+
+        [HttpPost]
+        public ActionResult CadastrarProjetado(projetado rece)
+        {
+            if (ModelState.IsValid)
+            {
+                projetado x = new projetado();
+                x.CadastrarProjetado(rece, Convert.ToInt32(Session["usuarioLogadoID"]));
+                return RedirectToAction("Projetado");
+            }
+            return RedirectToAction("Geral");
+        }
+
+
+        public ActionResult ProjetadoDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            projetado main = contexto.projetadoes.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+
+            projetado rece = new projetado();
+            rece.RemoverProjetado(main.ProjetadoID);
+
+            return RedirectToAction("Projetado");
+        }
+
+
+
+        public ActionResult ProjetadoUpdate(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            projetado rece = contexto.projetadoes.Find(id);
+            if (rece == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_ProjetadoUpdate", rece);
+        }
+
+        [HttpPost] // this action takes the viewModel from the modal
+        public ActionResult ProjetadoUpdate(projetado rece)
+        {
+
+            rece.UpdateProjetado(rece);
+            return RedirectToAction("Projetado");
+        }
+    }
+}
--- app_life/models/projetado.cs
+++ app_life/models/projetado.cs
@@ -1,17 +1,76 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace APP_Life.Models
 {
-    public partial class projetado
+    public partial class projetado : IEnumerable<projetado>
     {
         public int ProjetadoID { get; set; }
         public Nullable<int> UsuarioID { get; set; }
         public Nullable<int> CategoriaID { get; set; }
         public Nullable<float> Valor { get; set; }
         public string Descricao { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string Data { get; set; }
         public virtual categoria categoria { get; set; }
         public virtual usuario usuario { get; set; }
+
+        public void CadastrarProjetado(projetado rece, int id)
+        {
+            app_lifeContext contexto = new app_lifeContext();
+            rece.UsuarioID = id;
+            contexto.projetadoes.Add(rece);
+
+            contexto.SaveChanges();
+        }
+
+        public void RemoverProjetado(int id)
+        {
+            app_lifeContext contexto = new app_lifeContext();
+            projetado main = contexto.projetadoes.Find(id);
+            contexto.projetadoes.Remove(main);
+            contexto.SaveChanges();
+        }
+
+
+        public void UpdateProjetado(projetado rece)
+        {
+            app_lifeContext contexto = new app_lifeContext();
+            var query = from u in contexto.projetadoes where u.ProjetadoID == rece.ProjetadoID select u;
+            foreach (var item in query)
+            {
+                item.Descricao = rece.Descricao;
+                item.Valor = rece.Valor;
+                item.Data = rece.Data;
+                //item.categoria.nome = rece.categoria.nome;
+                item.UsuarioID = rece.UsuarioID;
+                item.CategoriaID = rece.CategoriaID;
+
+            }
+            contexto.SaveChanges();
+        }
+
+
+        List<projetado> projetadoLista;
+
+
+
+        public IEnumerator<projetado> GetEnumerator()
+        {
+            return projetadoLista.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+
     }
 }
